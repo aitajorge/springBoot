6701126d6c5691d4834cedb7f6e9c8deb6ee3203
aitajorge <aitajorge@yahoo.com.ar>
2022-12-15 15:05:42 -0300

    estaba incompleto, no era un error

--- src/main/java/com/miPrimeraApp/SpringBoot/Controller/Controller.java
+++ src/main/java/com/miPrimeraApp/SpringBoot/Controller/Controller.java
@@ -1,14 +1,19 @@
 package com.miPrimeraApp.SpringBoot.Controller;
-
+import com.miPrimeraApp.SpringBoot.model.Persona;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
 public class Controller {
          
-    
+    List<Persona> listaPersonas = new ArrayList();
     
     @GetMapping ("/hola/{nombre}/{apellido}/{edad}")   
     public String decirHola(@PathVariable String nombre,  
@@ -24,7 +29,16 @@ public class Controller {
         return "Chau Mundo " + nombre + " apellido: " + apellido + " Edad: " + edad;
     }
     
-    
-    
+  @PostMapping ("/new/persona")  
+  public void agregarPersona(@RequestBody Persona pers){
+      listaPersonas.add(pers);
+  }   
+
+  @GetMapping ("/ver/personas")
+  @ResponseBody
+  public List<Persona> verPersonas(){
+      return listaPersonas;
+  }
+  
 }
 
